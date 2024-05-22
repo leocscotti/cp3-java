56ba0466f8ad2efe655dfac26a1f8a5c22103934
Leo Scotti <126523762+leocscotti@users.noreply.github.com>
2024-05-22 19:20:59 -0300

    Update RevisaoapiApplication.java

--- src/main/java/br/com/fiap/revisaoapi/RevisaoapiApplication.java
+++ src/main/java/br/com/fiap/revisaoapi/RevisaoapiApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RevisaoapiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RevisaoapiApplication.class, args);
-    }
+        SpringApplication.run(RevisaoapiApplication.class, args);
+    }
 
 }
